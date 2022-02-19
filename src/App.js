01be65114a81f214eca10579d639e380150b71cd
shadowapril <aprildykj@gmail.com>
2022-02-19 18:57:18 +0800

    :fire: 完成echarts图表展示——结项撒花

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react'
 import {Redirect, Route, Switch} from 'react-router-dom';
-import { createBrowserHistory } from "history";
 
 import Login from './containers/login/login'
 import Admin from './containers/admin/admin'
 
 
-const history = createBrowserHistory();
 class App extends Component {
 
     render() {
